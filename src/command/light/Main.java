0fd8d86b30bde5e465ac68ad588e3bf6298569b5
Sergii Rubinov <sergii.rubinov@gmail.com>
2017-09-20 00:49:06 +0300

    Update Main.java
    add description

--- src/command/light/Main.java
+++ src/command/light/Main.java
@@ -2,11 +2,15 @@ package command.light;
 
 public class Main {
 	public static void main(String[] args){
+	      //sender
 	      TV tv = new TV();
 	      Light light = new Light();
 	      
+	      //command
 	      Command lightOf = new LightOffCommand(light);
 	      Command tvOn = new TvOnCommand(tv);
+	      
+	      //receiver	
 	      Switch s = new Switch(tvOn, lightOf);
 	      s.off();
 	      s.on();
